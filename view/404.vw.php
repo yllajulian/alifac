957d5145b2b8175443f0b4d0304e910333920ea3
yllajulian <yllajulian@gmail.com>
2022-06-14 18:11:06 -0500

    Se agrega el mensaje en error 404

--- view/404.vw.php
+++ view/404.vw.php
@@ -1,7 +1,7 @@
 <div class="overlay"></div>
 	<div class="terminal">
 		  <h1>Error <span class="errorcode">404</span></h1>
-		  <p class="output">La página que está buscando podría haber sido eliminada, ha
+		  <p class="output"> Nueva modificacionLa página que está buscando podría haber sido eliminada, ha
 			cambiado de Nombre  o no está disponible temporalmente</p>
 		   <p class="output">Por favor regrese al <a href="./?View=Inicio"> Inicio </a></p>
 		  <p class="output">Buena suerte</p>

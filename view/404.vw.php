70b3269f1ebc972fb2e348d44d84c62cae5be2c7
yllajulian <yllajulian@gmail.com>
2022-06-15 18:29:53 -0500

    tilulo cambiado de error 404

--- view/404.vw.php
+++ view/404.vw.php
@@ -1,5 +1,6 @@
 <div class="overlay"></div>
 	<div class="terminal">
+		<h2 class="col-12 bg-info-400"><span class="text-white"> jesus hora</span></h2>
 		  <h1>Error <span class="errorcode">404</span></h1>
 		  <p class="output">La página que está buscando podría haber sido eliminada, ha
 			cambiado de Nombre  o no está disponible temporalmente</p>
